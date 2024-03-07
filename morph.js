7fcb7d195e16b4c838bd833295489dd97d0945ba
clavinsh <klavins.ar@gmail.com>
2024-03-07 16:29:44 +0200

    Fixed polygon normalization issues, added vertex drawing

--- morph.js
+++ morph.js
@@ -59,6 +59,24 @@ class Polygon {
 
         ctx.closePath();
         ctx.fill();
+
+        Polygon.DrawVertices(ctx, polygon);
+    }
+
+    static DrawVertices(ctx, polygon) {
+        let points = polygon.points;
+
+        if(points.length === 0) {
+            return;
+        }
+
+        ctx.fillStyle = "red";
+
+        for(let i = 0; i < points.length; i++) {
+            ctx.fillRect(points[i].x,points[i].y,5,5);
+        }
+
+        ctx.fillStyle = "black";
     }
 
     // adjust the vertex array and sorts them, if needed,
@@ -94,19 +112,22 @@ class Polygon {
             DividePolygon(polygon1, Math.abs(pointDiff));
         }
 
-        // divides the given polygon diff  number of times by going round-n-round the vertices
+        // divides the given polygon 'diff' number of times by going round-n-round the vertices
         function DividePolygon(polygon, diff) {
             let firstEdgeSplitPoint = 0;
-            let secondEdgeSplitPoint = 1;
             
             for(let i = 0; i < diff; i++) {
+                let secondEdgeSplitPoint = (firstEdgeSplitPoint + 1) % (polygon.points.length);
+
+                console.log(firstEdgeSplitPoint, secondEdgeSplitPoint);
+
                 let xMidPoint = Math.round((polygon.points[secondEdgeSplitPoint].x + polygon.points[firstEdgeSplitPoint].x)/2);
                 let yMidPoint = Math.round((polygon.points[secondEdgeSplitPoint].y + polygon.points[firstEdgeSplitPoint].y)/2);
 
-                polygon.points.splice(firstEdgeSplitPoint, 0,  new Point(xMidPoint, yMidPoint));
+                // inserts the new vertex right after the first vertex
+                polygon.points.splice(firstEdgeSplitPoint + 1, 0,  new Point(xMidPoint, yMidPoint));
 
-                firstEdgeSplitPoint = (firstEdgeSplitPoint + 2) % (polygon.points.length);
-                secondEdgeSplitPoint = (firstEdgeSplitPoint + 1) % (polygon.points.length);
+                firstEdgeSplitPoint = (secondEdgeSplitPoint + 1) % (polygon.points.length);
             }
         }
     }
@@ -114,17 +135,15 @@ class Polygon {
     static MorphPolygons(ctx, polygon1, polygon2, duration = 10000) {
         
         const frameRate = 60;
-        const totalFrames = (duration / 1000) * frameRate;
+        const totalFrames = Math.round(frameRate *  duration / 1000);
         let frame = 0;
 
         let points1 = polygon1.points;
         let points2 = polygon2.points;
 
-        console.log(polygon1, polygon2);
         Polygon.NormalizePolygon(polygon1);
         Polygon.NormalizePolygon(polygon2);
         Polygon.EqualizePolygons(polygon1, polygon2);
-        console.log(polygon1, polygon2);
 
         const diffs = points1.map((point, i) => ({
             dx: (points2[i].x - point.x) / totalFrames,
@@ -135,6 +154,8 @@ class Polygon {
             if(frame < totalFrames) {
                 ctx.clearRect(0,0,canvas.width, canvas.height);
 
+                // linear interpolation to calculate the intermediate vertices
+                // between the two polygons
                 const intermediatePoints = points1.map((point, i) => new Point(
                     Math.round(point.x + diffs[i].dx * frame),
                     Math.round(point.y + diffs[i].dy * frame),
@@ -148,7 +169,6 @@ class Polygon {
                 //the final-final frame is the second polygon
                 Polygon.DrawPolygon(ctx, polygon2);
             }
-
         }
 
         animate();
