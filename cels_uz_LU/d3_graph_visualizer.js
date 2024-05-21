56cc7723110422333c3a20d136940bdc50370555
clavinsh <klavins.ar@gmail.com>
2024-05-21 20:31:27 +0300

    Temp using nodes as svgs, not working for now

--- cels_uz_LU/d3_graph_visualizer.js
+++ cels_uz_LU/d3_graph_visualizer.js
@@ -10,11 +10,11 @@ window.addEventListener("load", () => {
     const tooltip = d3.select(".tooltip");
 
     const nodes = [
-        { id: 1, name: "Home" },
-        { id: 2, name: "Bus Stop" },
-        { id: 3, name: "School" },
-        { id: 4, name: "Company" },
-        { id: 5, name: "Person" },
+        { id: 1, name: "Home", icon: "home.svg" },
+        { id: 2, name: "Bus Stop", icon: "home.svg" },
+        { id: 3, name: "School", icon: "home.svg" },
+        { id: 4, name: "Company", icon: "home.svg" },
+        { id: 5, name: "Person", icon: "home.svg" },
     ];
 
     const links = [
@@ -43,15 +43,21 @@ window.addEventListener("load", () => {
         .append("line")
         .attr("class", "link");
 
+    const container = svg.append("g").attr("class");
+
     const node = svg
         .append("g")
         .attr("class", "nodes")
         .selectAll("circle")
+        .selectAll("image")
         .data(nodes)
         .enter()
         .append("circle")
         .attr("class", "node")
         .attr("r", 10)
+        .attr("xlink:href", (d) => d.icon)
+        .attr("width", 32)
+        .attr("height", 32)
         .call(
             d3
                 .drag()
@@ -73,7 +79,7 @@ window.addEventListener("load", () => {
             .attr("x2", (d) => d.target.x)
             .attr("y2", (d) => d.target.y);
 
-        node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+        node.attr("x", (d) => d.x - 16).attr("y", (d) => d.y - 16);
     }
 
     function dragstarted(event, d) {
@@ -102,247 +108,5 @@ window.addEventListener("load", () => {
             .style("top", event.pageY - 28 + "px");
     }
 });
-
-function ForceGraph(
-    {
-        nodes, // an iterable of node objects (typically [{id}, …])
-        links, // an iterable of link objects (typically [{source, target}, …])
-    },
-    {
-        nodeId = (d) => d.id, // given d in nodes, returns a unique identifier (string)
-        nodeGroup, // given d in nodes, returns an (ordinal) value for color
-        nodeGroups, // an array of ordinal values representing the node groups
-        nodeTitle, // given d in nodes, a title string
-        nodeFill = "currentColor", // node stroke fill (if not using a group color encoding)
-        nodeStroke = "#fff", // node stroke color
-        nodeStrokeWidth = 1.5, // node stroke width, in pixels
-        nodeStrokeOpacity = 1, // node stroke opacity
-        nodeRadius = 5, // node radius, in pixels
-        nodeStrength,
-        linkSource = ({ source }) => source, // given d in links, returns a node identifier string
-        linkTarget = ({ target }) => target, // given d in links, returns a node identifier string
-        linkStroke = "#999", // link stroke color
-        linkStrokeOpacity = 0.6, // link stroke opacity
-        linkStrokeWidth = 1.5, // given d in links, returns a stroke width in pixels
-        linkStrokeLinecap = "round", // link stroke linecap
-        linkStrength,
-        colors = d3.schemeTableau10, // an array of color strings, for the node groups
-        width = 640, // outer width, in pixels
-        height = 400, // outer height, in pixels
-        invalidation, // when this promise resolves, stop the simulation
-    } = {}
-) {
-    // Compute values.
-    const N = d3.map(nodes, nodeId).map(intern);
-    const R =
-        typeof nodeRadius !== "function" ? null : d3.map(nodes, nodeRadius);
-    const LS = d3.map(links, linkSource).map(intern);
-    const LT = d3.map(links, linkTarget).map(intern);
-    if (nodeTitle === undefined) nodeTitle = (_, i) => N[i];
-    const T = nodeTitle == null ? null : d3.map(nodes, nodeTitle);
-    const G = nodeGroup == null ? null : d3.map(nodes, nodeGroup).map(intern);
-    const W =
-        typeof linkStrokeWidth !== "function"
-            ? null
-            : d3.map(links, linkStrokeWidth);
-    const L =
-        typeof linkStroke !== "function" ? null : d3.map(links, linkStroke);
-
-    // Replace the input nodes and links with mutable objects for the simulation.
-    nodes = d3.map(nodes, (_, i) => ({ id: N[i] }));
-    links = d3.map(links, (_, i) => ({ source: LS[i], target: LT[i] }));
-
-    // Compute default domains.
-    if (G && nodeGroups === undefined) nodeGroups = d3.sort(G);
-
-    // Construct the scales.
-    const color =
-        nodeGroup == null ? null : d3.scaleOrdinal(nodeGroups, colors);
-
-    // Construct the forces.
-    const forceNode = d3.forceManyBody();
-    const forceLink = d3.forceLink(links).id(({ index: i }) => N[i]);
-    if (nodeStrength !== undefined) forceNode.strength(nodeStrength);
-    if (linkStrength !== undefined) forceLink.strength(linkStrength);
-
-    const simulation = d3
-        .forceSimulation(nodes)
-        .force("link", forceLink)
-        .force("charge", forceNode)
-        .force("center", d3.forceCenter())
-        .on("tick", ticked);
-
-    const svg = d3
-        .create("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("viewBox", [-width / 2, -height / 2, width, height])
-        .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
-
-    const link = svg
-        .append("g")
-        .attr("stroke", typeof linkStroke !== "function" ? linkStroke : null)
-        .attr("stroke-opacity", linkStrokeOpacity)
-        .attr(
-            "stroke-width",
-            typeof linkStrokeWidth !== "function" ? linkStrokeWidth : null
-        )
-        .attr("stroke-linecap", linkStrokeLinecap)
-        .selectAll("line")
-        .data(links)
-        .join("line");
-
-    const node = svg
-        .append("g")
-        .attr("fill", nodeFill)
-        .attr("stroke", nodeStroke)
-        .attr("stroke-opacity", nodeStrokeOpacity)
-        .attr("stroke-width", nodeStrokeWidth)
-        .selectAll("circle")
-        .data(nodes)
-        .join("circle")
-        .attr("r", nodeRadius)
-        .call(drag(simulation));
-
-    if (W) link.attr("stroke-width", ({ index: i }) => W[i]);
-    if (L) link.attr("stroke", ({ index: i }) => L[i]);
-    if (G) node.attr("fill", ({ index: i }) => color(G[i]));
-    if (R) node.attr("r", ({ index: i }) => R[i]);
-    if (T) node.append("title").text(({ index: i }) => T[i]);
-    if (invalidation != null) invalidation.then(() => simulation.stop());
-
-    function intern(value) {
-        return value !== null && typeof value === "object"
-            ? value.valueOf()
-            : value;
-    }
-
-    function ticked() {
-        link.attr("x1", (d) => d.source.x)
-            .attr("y1", (d) => d.source.y)
-            .attr("x2", (d) => d.target.x)
-            .attr("y2", (d) => d.target.y);
-
-        node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
-    }
-
-    function drag(simulation) {
-        function dragstarted(event) {
-            if (!event.active) simulation.alphaTarget(0.3).restart();
-            event.subject.fx = event.subject.x;
-            event.subject.fy = event.subject.y;
-        }
-
-        function dragged(event) {
-            event.subject.fx = event.x;
-            event.subject.fy = event.y;
-        }
-
-        function dragended(event) {
-            if (!event.active) simulation.alphaTarget(0);
-            event.subject.fx = null;
-            event.subject.fy = null;
-        }
-
-        return d3
-            .drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended);
-    }
-
-    return Object.assign(svg.node(), { scales: { color } });
-}
-
-function SusGraph(data) {
-    const width = 928;
-    const height = 600;
-
-    const color = d3.scaleOrdinal(d3.schemeCategory10);
-
-    const links = data.links.map((d) => ({ ...d }));
-    const nodes = data.nodes.map((d) => ({ ...d }));
-
-    const simulation = d3
-        .forceSimulation(nodes)
-        .force(
-            "link",
-            d3.forceLink(data).id((d) => d.id)
-        )
-        .force("charge", d3.forceManyBody())
-        .force("center", d3.forceCenter(width / 2, height / 2))
-        .on("tick", ticked);
-
-    const svg = d3
-        .select("#graph")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("viewBox", [0, 0, width, height])
-        .attr("style", "max-width: 100%; height: auto;");
-
-    const link = svg
-        .append("g")
-        .attr("stroke", "#999")
-        .attr("stroke-opacity", 0.6)
-        .selectAll()
-        .data(links)
-        .join("line")
-        .attr("stroke-width", (d) => Math.sqrt(d.value));
-
-    const node = svg
-        .append("g")
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 1.5)
-        .selectAll()
-        .data(nodes)
-        .join("circle")
-        .attr("r", 5)
-        .attr("fill", (d) => color(d.group));
-
-    node.append("title").text((d) => d.id);
-
-    node.call(
-        d3
-            .drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended)
-    );
-
-    function ticked() {
-        link.attr("x1", (d) => d.source.x)
-            .attr("y1", (d) => d.source.y)
-            .attr("x2", (d) => d.target.x)
-            .attr("y2", (d) => d.target.y);
-
-        node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
-    }
-
-    function dragstarted(event) {
-        if (!event.active) simulation.alphaTarget(0.3).restart();
-        event.subject.fx = event.subject.x;
-        event.subject.fy = event.subject.y;
-    }
-
-    function dragged(event) {
-        event.subject.fx = event.x;
-        event.subject.fy = event.y;
-    }
-
-    // Restore the target alpha so the simulation cools after dragging ends.
-    // Unfix the subject position now that it’s no longer being dragged.
-    function dragended(event) {
-        if (!event.active) simulation.alphaTarget(0);
-        event.subject.fx = null;
-        event.subject.fy = null;
-    }
-
-    // When this cell is re-run, stop the previous simulation. (This doesn’t
-    // really matter since the target alpha is zero and the simulation will
-    // stop naturally, but it’s a good practice.)
-    //invalidation.then(() => simulation.stop());
-
-    return svg.node();
-}
 
 //SusGraph(inputData);
